8e79e5a4bec626130ad28e22399e553d3cae0151
KDSGYT <kdsgyt@gmail.com>
2020-11-07 08:35:03 -0500

    Displaying all the information available

--- src/components/AgencyItem.js
+++ src/components/AgencyItem.js
@@ -4,17 +4,17 @@ import stampIcon from '../assets/stamp.svg';
 const AgencyItem = ({ data, id }) => {
 
     const { name, phone, email, address, branch, website, verified } = data
-    const stamp = verified ? <img src={stampIcon} className="stamp" alt="verified" title="verified"/>: null;
+    const stamp = verified ? <img src={stampIcon} className="stamp" alt="verified" title="verified" /> : null;
     return (
         <div className="agency-item">
-            <li>{id}</li>    
+            <li>{id}</li>
             <li>{name} {stamp}</li>
             <li>{phone}</li>
-            {/* <li>{email}</li> */}
-            {/* <li>{address}</li> */}
+            <li>{email}</li>
+            <li>{address}</li>
             <li>{branch}</li>
-            {/* <li><a href={website}>{website}</a></li> */}
-            <br/>
+            <li><a href={website}>{website}</a></li>
+            <br />
         </div>
     )
 }
