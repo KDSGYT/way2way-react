d469ceabc7988a03969ca6ca52fcf2a670eee33f
KDSGYT <kdsgyt@gmail.com>
2020-12-06 22:14:23 -0500

    Now you can push data to the database

--- src/views/AddData/AddData.js
+++ src/views/AddData/AddData.js
@@ -1,41 +1,80 @@
-import React from 'react'
-import firebase, { firestore } from '../../Util/firebase'
+import { Checkbox, TextField, FormControlLabel } from '@material-ui/core';
+import React, { useRef } from 'react';
+import { firestore } from '../../Util/firebase';
+import './AddData.scss';
+
 export default function AddData() {
 
-    const [name, setName] = React.useState('');
-    const [phoneNumber, setphoneNumber] = React.useState('');
+    const name = useRef(null);
+    const phone = useRef(null)
+    const email = useRef(null)
+    const address = useRef(null)
+    const branch = useRef(null)
+    const fulltime = useRef(null)
+    const parttime = useRef(null)
+    const website = useRef(null)
+    const verified = useRef(null)
 
-    const handleNameChange = (e) => setName(e.target.value);
-    const handlePhoneChange = (e) => setphoneNumber(e.target.value);
-
-    // React.useEffect(() => {
-
-    // }, [name, phoneNumber])
-
+    /**
+     * Creates a entry in the database
+     * @param { node } e - the node that triggered the event
+     */
     const handleSubmit = async (e) => {
+
         e.preventDefault();
         const data = {
-            name,
-            phone: phoneNumber
+            name: name.current.value,
+            phone: phone.current.value,
+            email: email.current.value,
+            address: address.current.value,
+            branch: branch.current.value,
+            website: website.current.value,
+            parttime: parttime.current.checked,
+            fulltime: fulltime.current.checked,
+            verified: verified.current.checked
         }
         firestore.collection('agencies').doc().set(data)
-            .then ((res) => console.log("worked"))
+            .then((res) => console.log("worked"))
             .catch((e) => console.error("did"))
-        // const db = firestore.collection('users');
-        // db.get()
-        //     // .then((res) => res.json())
-        //     .then( res =>  console.log(res))
-        //     .catch(e => console.error(e))
     }
 
     return (
         <section id="add-data">
-            <h1>Add data to firebase</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Name</label>
-                <input type="text" value={name} onChange={handleNameChange} />
-                <label>Phone</label>
-                <input type="text" value={phoneNumber} onChange={handlePhoneChange} />
+            <h1 id="heading">Creating A New Entry</h1>
+            <form id="submission-form" autoComplete="off" onSubmit={handleSubmit} >
+                <TextField inputRef={name} id="agency-name" required label="Agency Name" />
+                <TextField inputRef={phone} id="phone" required label="Phone" />
+                <TextField inputRef={email} id="email" label="Email" />
+                <TextField inputRef={address} id="address" required label="Address" />
+                <TextField inputRef={branch} id="branch" required label="Branch" />
+                <TextField inputRef={website} id="website" label="Website" />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            inputRef={parttime}
+                            label="Part-time"
+                            inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+                    }
+                    label="Part-Time"
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            inputRef={fulltime}
+                            label="Full-time"
+                            inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+                    }
+                    label="Full-Time"
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            inputRef={verified}
+                            label="Verified"
+                            inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+                    }
+                    label="Verified"
+                />
                 <input type="submit" />
             </form>
         </section>
