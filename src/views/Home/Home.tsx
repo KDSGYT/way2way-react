bc50fe36f3cc179325555349ca458ab8e3356396
Karan Pal Singh <kdsgyt@gmail.com>
2021-02-20 19:26:46 -0500

    Doing animation:

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,4 +1,5 @@
-import React, { FC, } from 'react';
+import { Fade } from '@material-ui/core';
+import React, { FC, useEffect, useState, } from 'react';
 import AboutSection from '../../Components/AboutSection/AboutSection';
 import AccomodationSection from '../../Components/AccomodationSection/AccomodationSection';
 import AgencySection from '../../Components/AgencySection/AgencySection';
@@ -9,34 +10,25 @@ interface props {
 }
 
 const Home: FC<props> = () => {
-
-    // function homeProfileCallback(
-    //     id: any, // the "id" prop of the Profiler tree that has just committed
-    //     phase: any, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-    //     actualDuration: any, // time spent rendering the committed update
-    //     baseDuration: any, // estimated time to render the entire subtree without memoization
-    //     startTime: any, // when React began rendering this update
-    //     commitTime: any, // when React committed this update
-    //     interactions: any // the Set of interactions belonging to this update  
-    // ) {
-    //     const data = {
-    //         id, // the "id" prop of the Profiler tree that has just committed
-    //         phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-    //         actualDuration, // time spent rendering the committed update
-    //         baseDuration, // estimated time to render the entire subtree without memoization
-    //         startTime, // when React began rendering this update
-    //         commitTime, // when React committed this update
-    //         interactions // the Set of interactions belonging to this update
-    //     }
-    //     console.table(data)
-    // }
+    const [render, setRender] = useState(false);
+    useEffect(() => {
+        setRender(true)
+        return () => {
+            setRender(false)
+        };
+    }, []);
 
     return (
         <div id="home-page">
             <section id="home">
-                <div id="website-name">
-                    <h1>WAY2WAY.in</h1>
-                </div>
+                <Fade
+                    in={render}
+                    timeout={1000}
+                >
+                    <div id="website-name">
+                        <h1>WAY2WAY.in</h1>
+                    </div>
+                </Fade>
             </section>
             <AgencySection />
             <AccomodationSection />
